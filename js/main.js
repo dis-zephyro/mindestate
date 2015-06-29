1bf58633c910a170327c4ca7c73515b70bb42639
dis-zephyro <zephyro@yandex.ru>
2015-06-29 18:32:29 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// placeholders --------
+$(function(){
+    var mr_input="input[type=text]";
+    $(mr_input).each(function() {    $(this).data('stext',$(this).val());                                               });
+    $(mr_input).focus(function(){	 $(this).val()==$(this).data('stext')  ?   $(this).val(''): "0";	               	});
+    $(mr_input).blur(function() {    $(this).val().length<=0               ?   $(this).val($(this).data('stext')): "0"; });
+});
+
+
 // menu --------
 
 $(document).ready(function() {
@@ -33,22 +42,14 @@ $(function() {
         e.preventDefault();
         menu.slideToggle();
     });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 767 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
 });
-
-
-
-$(function(){
-    var navHeight = $('.sidenav_inner').height();
-    var browserHeight = $(window).height();
-    console.log(navHeight);
-    console.log(browserHeight);
-
-    $(window).on("scroll", function() {
-        if ($(window).scrollTop() > (navHeight - browserHeight)) $('.sidenav_inner').addClass('sidenav_fixed');
-        else $('.sidenav_inner').removeClass('sidenav_fixed');
-    })
-});
-
 
 
 // Collapse panel_main
@@ -58,7 +59,6 @@ $('.collapse_link').click(function (event) {
     var panel = $(this).closest('div.panel_main');
     var icon = $(this).find('i:first');
     var body = panel.find('div.panel_body');
-    var footer = panel.find('div.panel_footer');
     body.slideToggle(300);
 
 
@@ -74,8 +74,20 @@ $('.collapse_link').click(function (event) {
 
 // Close panel_main
 
-$('.close_link').click(function (event) {
-    event.preventDefault();
-    var panel = $(this).closest('div.panel_main');
-    panel.remove();
+
+$('input[type="checkbox"]').click(function() {
+    var rows = $(this).closest('tr');
+    var check = rows.find('span.check i');
+    var checkbox = ($(this).is(':checked'));
+
+    console.log(checkbox);
+
+    if (checkbox) {
+        check.addClass('text-success');
+        console.log('зеленый');
+    }
+    else{
+        check.removeClass('text-success');
+        console.log('серый');
+    }
 });

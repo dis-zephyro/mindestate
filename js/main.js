e6f5e11019720a6f9e6aa9db435dd30d74dc3852
dis-zephyro <zephyro@yandex.ru>
2015-06-29 14:02:57 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -37,6 +37,20 @@ $(function() {
 
 
 
+$(function(){
+    var navHeight = $('.sidenav_inner').height();
+    var browserHeight = $(window).height();
+    console.log(navHeight);
+    console.log(browserHeight);
+
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > (navHeight - browserHeight)) $('.sidenav_inner').addClass('sidenav_fixed');
+        else $('.sidenav_inner').removeClass('sidenav_fixed');
+    })
+});
+
+
+
 // Collapse panel_main
 
 $('.collapse_link').click(function (event) {
